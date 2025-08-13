bdc81e52f4247074ba3181ecfab164bdcbd8ca71
Tushar <ranpariyatushar88@gmail.com>
2025-08-13 19:43:59 +0530

    apiError, apiResponse

---
+++ project/src/index.js
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+import { app } from './app.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.on("error", (error) => {
+        console.log("error: ", error);
+        
+    })
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`app is running on serveer: ${process.env.PORT}`);
+
+    })
+})
+.catch((error) => {
+    console.log("Database connection failed ", error);
+})
+
+
+
+
+
+
+
+
+
+/*
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERRROR:", error);
+            throw error 
+            
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`app is listening on ${process.env.PORT}`);
+            
+        })
+
+    } catch(error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})() */
